2ed69df29349c7d464ef18c43b38ef99570cc3dd
Anton Frattaroli <afrattar@cisco.com>
2021-07-30 14:45:46 -0400

    fix: use gh actions master branch name in zeronorth script (#49)

--- scripts/build.sh
+++ scripts/build.sh
@@ -32,35 +32,18 @@ function build-and-publish-package {
   # Run Vulnerability Scan in the artifact using ZeroNorth - INT only
   if [ "${PKG_TYPE}" == "int" ]; then
     echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-    sudo docker pull zeronorth/owasp-5-job-runner
-    sudo docker run -v /home/travis/build/threatgrid/securex-news:/code/ -e CYBRIC_API_KEY="${CYBRIC_API_KEY}" -e POLICY_ID=JoJaSviQSdSrX2LdiM2KDA -e WORKSPACE=/home/travis/build/threatgrid/securex-news -v /var/run/docker.sock:/var/run/docker.sock --name zeronorth zeronorth/integration:latest python cybric.py
+    docker pull zeronorth/owasp-5-job-runner
+    docker run -v /home/travis/build/threatgrid/securex-news:/code/ -e CYBRIC_API_KEY="${CYBRIC_API_KEY}" -e POLICY_ID=JoJaSviQSdSrX2LdiM2KDA -e WORKSPACE=/home/travis/build/threatgrid/securex-news -v /var/run/docker.sock:/var/run/docker.sock --name zeronorth zeronorth/integration:latest python cybric.py
     echo "Waiting the ZeroNorth Vulnerability Scanner to finish..."
     while [[ ! -z $(docker ps -a --format "{{.ID}}" -f status=running -f ancestor=zeronorth/owasp-5-job-runner) ]]; do sleep 5; done
   fi
 }
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  if [[ ${TRAVIS_BRANCH} == "master" ]]; then
+  if [[ ${TRAVIS_BRANCH} == "refs/heads/master" ]]; then
     # non-pr builds on the master branch yield INT packages
     echo "OK: master branch detected"
     build-and-publish-package "int"
-
-  elif [[ ${TRAVIS_BRANCH} == "release" ]]; then
-    # non-pr builds on 'release' branch yield REL packages
-    echo "OK: release branch detected using regex"
-    build-and-publish-package "rel"
-
-  elif [[ ${TRAVIS_BRANCH} =~ ^rel-[0-9]{4}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$ ]]; then
-    # non-pr builds on 'rel-yyyymmdd' branches yield REL packages
-    # To be removed at a future date, depending on the success of the new method
-    echo "OK: release branch detected using regex"
-    build-and-publish-package "rel"
-
-  elif [[ ${TRAVIS_BRANCH} =~ ^v[0-9]+(.[0-9]+)+$ ]]; then
-    # non-pr builds on 'v?.?' branches yield REL packages
-    # To be removed at a future date, once we're sure the process works.
-    echo "OK: v branch detected using regex"
-    build-and-publish-package "rel"
 
   else
     echo "Not on master or release branch. Not building a package."
